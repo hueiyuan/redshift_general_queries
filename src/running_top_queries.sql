fca3f682d9c0e442ce4c9712de4d038a568f20b9
Mars.Su <49678319+hueiyuan@users.noreply.github.com>
2022-07-07 15:05:13 +0800

    Update keyword as uppercase

--- src/running_top_queries.sql
+++ src/running_top_queries.sql
@@ -17,39 +17,39 @@ Columns:
    sum_spec_mb:		Sum of Megabytes scanned by a Spectrum query on that queue/hour
 **********************************************************************************************/
 
-select trim(database) as DB, 
-        count(query) as n_qry, 
-        max(substring (qrytext,1,80)) as qrytext, 
-        min(run_seconds) as "min" , 
-        max(run_seconds) as "max", 
-        avg(run_seconds) as "avg", 
-        sum(run_seconds) as total,  
-        max(query) as max_query_id, 
-        max(starttime)::date as last_run, 
+SELECT trim(databASe) AS DB, 
+        count(query) AS n_qry, 
+        max(substring (qrytext,1,80)) AS qrytext, 
+        min(run_seconds) AS "min" , 
+        max(run_seconds) AS "max", 
+        avg(run_seconds) AS "avg", 
+        sum(run_seconds) AS total,  
+        max(query) AS max_query_id, 
+        max(starttime)::date AS lASt_run, 
         aborted,
-        listagg(event, ', ') within group (order by query) as events
-from (
-    select userid,
+        listagg(event, ', ') WITHIN group (ORDER BY query) AS events
+FROM (
+    SELECT userid,
             label,
             stl_query.query,
-            trim(database) as database,
-            trim(querytxt) as qrytext,
-            md5(trim(querytxt)) as qry_md5,
+            trim(databASe) AS databASe,
+            trim(querytxt) AS qrytext,
+            md5(trim(querytxt)) AS qry_md5,
             starttime, endtime, 
-            datediff(seconds, starttime,endtime)::numeric(12,2) as run_seconds, 
+            datediff(seconds, starttime,endtime)::numeric(12,2) AS run_seconds, 
             aborted, 
-            decode(alrt.event,'Very selective query filter','Filter','Scanned a large number of deleted rows','Deleted','Nested Loop Join in the query plan','Nested Loop','Distributed a large number of rows across the network','Distributed','Broadcasted a large number of rows across the network','Broadcast','Missing query planner statistics','Stats',alrt.event) as event
-    from stl_query 
-    left outer join ( 
-        select query, trim(split_part(event,':',1)) as event 
-        from STL_ALERT_EVENT_LOG 
-        where event_time >=  dateadd(day, -7, current_Date)  
-        group by query, trim(split_part(event,':',1)) 
-    ) as alrt on alrt.query = stl_query.query
-    where userid <> 1 
-    -- and (querytxt like 'SELECT%' or querytxt like 'select%' ) 
-    -- and database = ''
-    and starttime >=  dateadd(day, -7, current_Date)
+            decode(alrt.event,'Very SELECTive query filter','Filter','Scanned a large number of deleted rows','Deleted','Nested Loop Join in the query plan','Nested Loop','Distributed a large number of rows across the network','Distributed','BroadcASted a large number of rows across the network','BroadcASt','Missing query planner statistics','Stats',alrt.event) AS event
+    FROM stl_query 
+    LEFT OUTER JOIN ( 
+        SELECT query, trim(split_part(event,':',1)) AS event 
+        FROM STL_ALERT_EVENT_LOG 
+        WHERE event_time >=  dateadd(day, -7, current_Date)  
+        GROUP BY query, trim(split_part(event,':',1)) 
+    ) AS alrt on alrt.query = stl_query.query
+    WHERE userid <> 1 
+    -- AND (querytxt like 'SELECT%' or querytxt like 'SELECT%' ) 
+    -- AND databASe = ''
+    AND starttime >=  dateadd(day, -7, current_Date)
 ) 
-group by database, label, qry_md5, aborted
-order by total desc limit 50;
+GROUP BY databASe, label, qry_md5, aborted
+ORDER BY total DESC LIMIT 50;
