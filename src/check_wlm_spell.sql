043c2afe8c2d3a0a1957994915e16620cbdecd70
Mars.Su <49678319+hueiyuan@users.noreply.github.com>
2022-07-07 14:33:53 +0800

    Update check_wlm_spell.sql

--- src/check_wlm_spell.sql
+++ src/check_wlm_spell.sql
@@ -1,5 +1,19 @@
 /**********************************************************************************************
 the query can check wlm whether have spell throught is_diskbased, blocks_to_disk and max_blocks_to_disk fields
+
+WLM service class ids
+
+|     ID     |                       Service Class                                  |
+|------------|----------------------------------------------------------------------|
+|    1-4     | Reserved for system use.                                             |
+|    5       | Used by the superuser queue.                                         |
+|    6-13    | Used by manual WLM queues that are defined in the WLM configuration. |
+|    14      | Used by short query acceleration.                                    |
+|    15      | Reserved for maintenance activities run by Amazon Redshift.          |
+|   100-107  | Used by automatic WLM queue when auto_wlm is true.                   |
+
+Reference: https://docs.aws.amazon.com/redshift/latest/dg/cm-c-wlm-system-tables-and-views.html#wlm-service-class-ids
+
 **********************************************************************************************/
 
 select s2.service_class, 
