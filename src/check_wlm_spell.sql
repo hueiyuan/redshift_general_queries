095f8ee82c4b84d1f2dd1bff88a7e8accb927234
Mars.Su <49678319+hueiyuan@users.noreply.github.com>
2022-07-07 14:28:48 +0800

    Update check_wlm_spell.sql

--- src/check_wlm_spell.sql
+++ src/check_wlm_spell.sql
@@ -2,7 +2,16 @@
 the query can check wlm whether have spell throught is_diskbased, blocks_to_disk and max_blocks_to_disk fields
 **********************************************************************************************/
 
-select s2.service_class, s2.service_class_name, s2.blocks_to_disk, s2.max_blocks_to_disk, s1.query, s1.step, s1.rows, s1.workmem, s1.label, s1.is_diskbased
+select s2.service_class, 
+      s2.service_class_name, 
+      s2.blocks_to_disk, 
+      s2.max_blocks_to_disk, 
+      s1.query, 
+      s1.step, 
+      s1.rows, 
+      s1.workmem, 
+      s1.label, 
+      s1.is_diskbased
 from svl_query_summary as s1
 join stl_query_metrics s2 on s1.query=s2.query
 order by workmem desc;
